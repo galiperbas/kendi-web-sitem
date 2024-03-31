073f6bb2b3a4805dfe000069ba853c72d150f9b5
galiperbas <149431061+galiperbas@users.noreply.github.com>
2024-03-31 14:12:12 +0300

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# kendi-web-sitem
+
+1.  temel kurulumlar: next.js, tailwindcss ve vercel ile deploy
+
+2. 
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function HomePage() {
+  return (
+    <div className="text-blue-400 bg-amber-100">Welcome to Next.js! ulen</div>
+  );
+}
+
+export default HomePage;
